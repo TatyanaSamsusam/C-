410b3a219c0636eec1bf6d7eaf90f855f72d59f2
Tatyana <android87@list.ru>
2022-02-27 20:57:05 +0300

    add condition If Else

---
+++ examples/example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Insert your name: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, it's MASHA!");
+}
+else
+{
+    Console.Write("hello, ");
+    Console.Write(username);
+}
