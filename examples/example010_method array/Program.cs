12a3a8269fdf0fb3e73df6349eb4a75f2e6ffa53
Tatyana <android87@list.ru>
2022-03-10 14:56:02 +0300

    added method array

---
+++ examples/example010_method array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {15, 201, 329, 112, 23, 133, 18, 55, 133, 28};
+
+int n = array.Length;
+int find = 133;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
