ce72ffc8166500abb71465a404202dcf34255f3c
Tatyana <android87@list.ru>
2022-03-19 12:36:12 +0300

    made cycle for

---
+++ examples/example012_for/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string result = Method4(10, "z");
+Console.WriteLine(result);
